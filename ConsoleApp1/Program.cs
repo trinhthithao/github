8a21a01d875c69f58d2ce46d14b91baadaac0830
thao <thaottd00576@fpt.edu.vn>
2018-05-31 10:27:12 +0700

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static List<Student> list = new List<Student>();
+
+        static void Main(string[] args)
+        {
+            GenerateMenu();
+        }
+        
+        private static void SearchByName()
+        {
+            Console.WriteLine("Please enter name to search: ");
+            var searchKey = Console.ReadLine();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Student student = list[i];
+                if (student.Name == searchKey)
+                {
+                    Console.WriteLine("Found: ");
+                    Console.WriteLine(student.RollNumber + " - " + student.Name);                    
+                }
+            }
+        }
+
+        private static void AddStudent()
+        {
+            var student = new Student();
+            Console.WriteLine("Please enter student rollnumber: ");
+            student.RollNumber = Console.ReadLine();
+            Console.WriteLine("Please enter student name: ");
+            student.Name = Console.ReadLine();
+            Console.WriteLine("Please enter student email: ");
+            student.Email = Console.ReadLine();
+            Console.WriteLine("Please enter student phone: ");
+            student.Phone = Console.ReadLine();
+            list.Add(student);
+        }
+
+        private static void DisplayStudent()
+        {
+            foreach (var student in list)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+
+        private static void GenerateMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("----------Quản lý sinh viên---------");
+                Console.WriteLine("1. Thêm mới sinh viên.");
+                Console.WriteLine("2. Hiển thị danh sách.");
+                Console.WriteLine("3. Tìm kiếm theo tên.");
+                Console.WriteLine("4. Thoát.");
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("Nhập lựa chọn của bạn: ");
+                var choice = Int32.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        AddStudent();
+                        break;
+                    case 2:
+                        DisplayStudent();
+                        break;
+                    case 3:
+                        SearchByName();
+                        break;
+                    case 4:
+                        Environment.Exit(1);
+                        break;
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        private string _rollNumber;
+        private string _name;
+        private string _email;
+        private string _phone;
+
+        public override string ToString()
+        {
+            return "Name: " + this.Name + " Email: " + this.Email;
+        }
+
+        public Student(string rollNumber, string name, string email, string phone)
+        {
+            this._rollNumber = rollNumber;
+            this._name = name;
+            this._email = email;
+            this._phone = phone;
+        }
+
+        public Student()
+        {
+        }
+
+        public string RollNumber
+        {
+            get => _rollNumber;
+            set => _rollNumber = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+
+        public string Phone
+        {
+            get => _phone;
+            set => _phone = value;
+        }
+
+//        static void Main(string[] args)
+//        {
+//            var dbCon = DBConnection.Instance();
+//            dbCon.DatabaseName = "student_manager";
+//            if (dbCon.IsConnect())
+//            {
+//                //suppose col0 and col1 are defined as VARCHAR in the DB
+//                string query = "SELECT * FROM product";
+//                var cmd = new MySqlCommand(query, dbCon.Connection);
+//                var reader = cmd.ExecuteReader();
+//                while(reader.Read())
+//                {
+//                    string someStringFromColumnZero = reader.GetString(0);
+//                    string someStringFromColumnOne = reader.GetString(1);
+//                    Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+//                }
+//                dbCon.Close();
+//            }
+//        }
+        
+    }
+}
